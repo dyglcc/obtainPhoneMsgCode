1169a8f927025daeefbaeb8caad0247a7ff5fe76
dyglcc <15901418657@163.com>
2020-04-27 09:13:58 +0800

    add

--- app/src/main/java/com/kuaishan/obtainmsg/ui/viewpager/MyShareFragment.java
+++ app/src/main/java/com/kuaishan/obtainmsg/ui/viewpager/MyShareFragment.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.kuaishan.obtainmsg.R;
@@ -17,7 +19,9 @@ public class MyShareFragment extends LazyFragment {
     private String tabName;
     private int position;
     private TextView textView;
-//    private ProgressBar progressBar;
+    private ListView list;
+    private ProgressBar progressBar;
+    private View stubEmpty;
 
     @Override
     protected View getPreviewLayout(LayoutInflater inflater, ViewGroup container) {
@@ -29,25 +33,31 @@ public class MyShareFragment extends LazyFragment {
         super.onCreateViewLazy(savedInstanceState);
         tabName = getArguments().getString(INTENT_STRING_TABNAME);
         position = getArguments().getInt(INTENT_INT_POSITION);
-        setContentView(R.layout.fragment_tabmain_item);
+        setContentView(R.layout.layout_myshare);
         textView = (TextView) findViewById(R.id.fragment_mainTab_item_textView);
         textView.setText(tabName + " " + position + " 界面加载完毕");
-//        progressBar = (ProgressBar) findViewById(R.id.fragment_mainTab_item_progressBar);
-        handler.sendEmptyMessage(1);
+        progressBar = (ProgressBar) findViewById(R.id.fragment_mainTab_item_progressBar);
+        stubEmpty = findViewById(R.id.layout_myshare);
+//        handler.sendEmptyMessage(1);
     }
 
     @Override
     protected void onDestroyViewLazy() {
         super.onDestroyViewLazy();
-        handler.removeMessages(1);
+//        handler.removeMessages(1);
     }
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(android.os.Message msg) {
-            textView.setVisibility(View.VISIBLE);
-//            progressBar.setVisibility(View.GONE);
+            if (msg.what == 1) {
+                progressBar.setVisibility(View.GONE);
+            }
+            if(msg.what ==10){
+                stubEmpty.setVisibility(View.VISIBLE);
+            }
+//            textView.setVisibility(View.VISIBLE);
         }
     };
 }
